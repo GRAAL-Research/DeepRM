1404501d232e9ac2276ceb8dcd93d1a89197b519
Benjamin Leblanc <beni-leblanc@hotmail.com>
2024-08-01 08:43:41 -0400

    "outp" wasn't squeezed.

--- src/model/mlp.py
+++ src/model/mlp.py
@@ -12,12 +12,13 @@ from src.model.utils.sign_straight_through import SignStraightThrough
 class MLP(nn.Module):
     def __init__(self, input_dim: int, hidden_dims: list[int], device: str, has_skip_connection: bool,
                  has_batch_norm: bool, batch_norm_min_dim: int, init_scheme: str = None,
-                 msg_type: str | None = None) -> None:
+                 msg_type: str | None = None, follows_msg: bool = False) -> None:
         super(MLP, self).__init__()
         self.has_skip_connection = has_skip_connection
 
         input_and_hidden_dims = MLP.compute_input_and_hidden_dims(input_dim, hidden_dims)
-        self.mlp = MLP.create_mlp(has_batch_norm, batch_norm_min_dim, msg_type, input_and_hidden_dims, device)
+        self.mlp = MLP.create_mlp(has_batch_norm, batch_norm_min_dim, msg_type,
+                                  follows_msg, input_and_hidden_dims, device)
 
         last_layer_idx = len(self.mlp) - 1
         self.skip_position = last_layer_idx - has_batch_norm
@@ -38,13 +39,11 @@ class MLP(nn.Module):
         return input_and_hidden_dims
 
     @staticmethod
-    def create_mlp(has_batch_norm: bool, batch_norm_min_dim: int, msg_type: str, input_and_hidden_dims: list[int],
-                   device: str = "cpu") -> nn.ModuleList:
+    def create_mlp(has_batch_norm: bool, batch_norm_min_dim: int, msg_type: str, follows_msg: bool,
+                   input_and_hidden_dims: list[int], device: str = "cpu") -> nn.ModuleList:
         modules = torch.nn.ModuleList()
         for dim_idx in range(len(input_and_hidden_dims) - 1):
-            if (has_batch_norm and
-                    batch_norm_min_dim <= input_and_hidden_dims[dim_idx] and
-                    (msg_type is None or dim_idx > 0)):
+            if has_batch_norm and batch_norm_min_dim <= input_and_hidden_dims[dim_idx] and not follows_msg:
                 modules.append(LazyBatchNorm(device))
             modules.append(nn.Linear(input_and_hidden_dims[dim_idx], input_and_hidden_dims[dim_idx + 1]))
 
