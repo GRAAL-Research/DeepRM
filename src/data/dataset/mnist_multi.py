18b77a8a6e638961243f68882fee3d3fb7013391
Benjamin Leblanc <beni-leblanc@hotmail.com>
2024-08-13 10:04:28 -0400

    add new dataset

--- src/data/dataset/mnist_multi.py
+++ src/data/dataset/mnist_multi.py
@@ -40,8 +40,9 @@ def create_datasets_cache_path(config: dict) -> Path:
 
 
 def create_and_store_mnist_datasets(config: dict) -> np.ndarray:
-    dataset = create_train_set(config)
-    datasets = create_mnist_binary_datasets(config, dataset)
+    train_dataset = create_train_set(config)
+    test_dataset = create_test_set(config)
+    datasets = create_mnist_binary_datasets(config, train_dataset, test_dataset)
     store_mnist_datasets(config, datasets)
 
     return datasets
@@ -52,27 +53,54 @@ def store_mnist_datasets(config: dict, datasets: np.ndarray) -> None:
     np.save(create_datasets_cache_path(config), datasets)
 
 
-def create_mnist_binary_datasets(config: dict, dataset) -> np.ndarray:
+def create_mnist_binary_datasets(config: dict, train_dataset, test_dataset) -> np.ndarray:
     binary_datasets = np.zeros((config["n_dataset"], config["n_instances_per_dataset"], config["n_features"] +
                                 config["target_size"]))
-    dataset = dataset[:config["n_instances_per_dataset"]]
-    dataset = torch.hstack((dataset[:, :-1], F.one_hot(dataset[:, -1]) * 2 - 1))
-    for binary_dataset_idx in range(config["n_dataset"]):
-        if binary_dataset_idx == config["n_dataset"]:
-            break
-        idx = np.arange(len(dataset))
+    train_dataset = torch.hstack((train_dataset[:, :-1], F.one_hot(train_dataset[:, -1]) * 2 - 1))
+    test_dataset = torch.hstack((test_dataset[:, :-1], F.one_hot(test_dataset[:, -1]) * 2 - 1))
+    print(config["n_dataset"])
+    n_swap = 10
+    n_partition = len(train_dataset) // config["n_instances_per_dataset"]
+    for num_swap in range(n_swap):
+        idx = np.arange(len(train_dataset))
         np.random.shuffle(idx)
-        dataset = dataset[idx]
-        dataset_shuffled = dataset.clone()
-        for i in range(config["n_pixels_to_permute"]):
-            first_pixel_location = np.random.randint(low=0, high=config["n_features"])
-            second_pixel_location = np.random.randint(low=0, high=config["n_features"])
-            first_pixel = dataset_shuffled[:, first_pixel_location].clone()
-            second_pixel = dataset_shuffled[:, second_pixel_location].clone()
-            dataset_shuffled[:, second_pixel_location] = first_pixel
-            dataset_shuffled[:, first_pixel_location] = second_pixel
-        binary_datasets[binary_dataset_idx] = dataset_shuffled
-        binary_dataset_idx += 1
+        train_dataset_shuffled = train_dataset[idx].clone()
+
+        pixels_idx = np.arange(config["n_features"])
+        np.random.shuffle(pixels_idx)
+        first_pixels_location = pixels_idx[:config["n_pixels_to_permute"] // 2]
+        second_pixels_location = pixels_idx[config["n_pixels_to_permute"] // 2:config["n_pixels_to_permute"]]
+        first_pixel = train_dataset_shuffled[:, first_pixels_location].clone()
+        second_pixel = train_dataset_shuffled[:, second_pixels_location].clone()
+        train_dataset_shuffled[:, second_pixels_location] = first_pixel
+        train_dataset_shuffled[:, first_pixels_location] = second_pixel
+        for num_partition in range(n_partition):
+            if n_partition * num_swap + num_partition == int(config["n_dataset"] * (config["splits"][0] + config["splits"][1])):
+                break
+            train_dataset_reduced = train_dataset_shuffled[num_partition * config["n_instances_per_dataset"]:
+                                                           (num_partition + 1) * config["n_instances_per_dataset"]]
+            binary_datasets[n_partition * num_swap + num_partition] = train_dataset_reduced
+        if n_partition * num_swap + num_partition == int(
+                config["n_dataset"] * (config["splits"][0] + config["splits"][1])):
+            break
+    train_idx = np.arange(int(config["n_dataset"] * (config["splits"][0] + config["splits"][1])))
+    np.random.shuffle(train_idx)
+    binary_datasets[np.arange(len(train_idx))] = binary_datasets[train_idx]
+    for binary_dataset_idx in range(int(config["n_dataset"] * (config["splits"][0] + config["splits"][1])),
+                                    config["n_dataset"]):
+        idx = np.arange(len(test_dataset))
+        np.random.shuffle(idx)
+        test_dataset_reduced = test_dataset[idx[:config["n_instances_per_dataset"]]]
+
+        pixels_idx = np.arange(config["n_features"])
+        np.random.shuffle(pixels_idx)
+        first_pixels_location = pixels_idx[:config["n_pixels_to_permute"] // 2]
+        second_pixels_location = pixels_idx[config["n_pixels_to_permute"] // 2:config["n_pixels_to_permute"]]
+        first_pixel = test_dataset_reduced[:, first_pixels_location].clone()
+        second_pixel = test_dataset_reduced[:, second_pixels_location].clone()
+        test_dataset_reduced[:, second_pixels_location] = first_pixel
+        test_dataset_reduced[:, first_pixels_location] = second_pixel
+        binary_datasets[binary_dataset_idx] = test_dataset_reduced
     return binary_datasets
 
 
@@ -85,7 +113,6 @@ def obtain_mnist_dataset(config: dict) -> torch.Tensor:
 
 def create_train_set(config: dict) -> torch.Tensor:
     train_set = torchvision.datasets.MNIST(root=str(MNIST_BASE_PATH), train=True, download=True)
-    train_set = apply_transforms_to_dataset(config, train_set)
     n_instances_in_mnist_train_set = train_set.data.shape[0]
     data = train_set.data.reshape((n_instances_in_mnist_train_set, config["n_features"]))
     target = train_set.targets.reshape(n_instances_in_mnist_train_set, -1)
@@ -95,30 +122,7 @@ def create_train_set(config: dict) -> torch.Tensor:
 
 def create_test_set(config: dict) -> torch.Tensor:
     test_set = torchvision.datasets.MNIST(root=str(MNIST_BASE_PATH), train=False, download=True)
-    test_set = apply_transforms_to_dataset(config, test_set)
     n_instances_in_mnist_test_set = test_set.data.shape[0]
 
     return torch.hstack((test_set.data.reshape((n_instances_in_mnist_test_set, config["n_features"])),
                          test_set.targets.reshape(n_instances_in_mnist_test_set, -1)))
-
-
-def apply_transforms_to_dataset(config: dict, dataset: MNIST) -> MNIST:
-    square_root_of_n_features = np.sqrt(config["n_features"])
-    is_a_perfect_square = 0 <= config["n_features"] == int(square_root_of_n_features) ** 2
-    assert is_a_perfect_square, "The number of features must be a perfect square."
-
-    new_img_size = (int(square_root_of_n_features), int(square_root_of_n_features))
-
-    if new_img_size == MNIST_DEFAULT_IMG_SIZE:
-        return dataset
-
-    transform = transforms.Compose([to_pil_image, transforms.Resize(new_img_size), ToTensor()])
-
-    transformed_data = []
-    for img in tqdm(dataset.data, desc="Applying MNIST's transforms"):
-        img = transform(img)
-        transformed_data.append(img)
-
-    dataset.data = torch.stack(transformed_data).squeeze(1)
-
-    return dataset
