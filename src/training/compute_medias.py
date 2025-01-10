aa3bebb6aec724937d0da3b557101b852c420535
beleb91 <beni-leblanc@hotmail.com>
2025-01-10 15:44:23 -0500

    [TODO] Performance matrix computation can't be made since datasets can be of various lenghts.

--- src/training/compute_medias.py
+++ src/training/compute_medias.py
@@ -14,8 +14,8 @@ def compute_medias(config: dict, meta_predictor: SimpleMetaNet, test_loader: Dat
     if config["n_features"] == 2 and config["is_using_wandb"]:
         show_decision_boundaries(meta_predictor, config["dataset"], test_loader, predictor, wandb, config["device"])
 
-    if config["dataset"] in ["mnist", "cifar100_binary"]:
+    if config["dataset"] in ["mnist", "cifar100"]:
         idx = [train_idx, valid_idx, test_idx]
-        show_performance_matrix(meta_predictor, predictor, config["dataset"], datasets, classes_name, idx,
-                                config["n_dataset"], config["is_using_wandb"], wandb, config["batch_size"],
-                                config["are_test_classes_shared_with_train"], config["device"])
+        #show_performance_matrix(meta_predictor, predictor, config["dataset"], datasets, classes_name, idx,
+        #                        config["n_dataset"], config["is_using_wandb"], wandb, config["meta_batch_size"],
+        #                        config["are_test_classes_shared_with_train"], config["device"])
