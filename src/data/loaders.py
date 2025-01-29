3d2fe2265ca3018877aabd38beb6174c7800f91e
beleb91 <beni-leblanc@hotmail.com>
2025-01-29 11:21:36 -0500

    [PATCH] Tensors not on same device when "cpu" is chosen.

--- src/data/loaders.py
+++ src/data/loaders.py
@@ -96,12 +96,11 @@ def collate_fn_padd(batch):
     note: it converts things ToTensor manually here since the ToTensor transform
     assume it takes in images rather than arbitrary tensors.
     '''
-    device = "cuda:0"
     ## get sequence lengths
-    lengths = torch.tensor([ t.shape[0] for t in batch ]).to(device)
+    lengths = torch.tensor([ t.shape[0] for t in batch ])
     ## padd
-    batch = [ torch.Tensor(t).to(device) for t in batch ]
+    batch = [ torch.Tensor(t) for t in batch ]
     batch = torch.nn.utils.rnn.pad_sequence(batch)
     ## compute mask
-    mask = (batch != 0).to(device)
+    mask = (batch != 0)
     return torch.transpose(batch, 0, 1), lengths, mask
